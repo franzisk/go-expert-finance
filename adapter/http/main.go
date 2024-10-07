0f0e76ac6abd5e2c60a8a4e58b9082b568454611
franzisk <francisco.vieira.souza@gmail.com>
2024-10-07 16:28:38 -0300

    Adicionado o Prometheus

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -5,13 +5,18 @@ import (
 
 	"github.com/franzisk/go-expert-finance/adapter/http/actuator"
 	"github.com/franzisk/go-expert-finance/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Init serve para iniciar o serviço REST da aplicação
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
+
 	http.HandleFunc("/transactions/create", transaction.CraeteTransaction)
+
 	http.HandleFunc("/health", actuator.HealthCheck)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 }
